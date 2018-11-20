8ccb79f29ea5615146ad0b4a22e38860e8d0fc8f
LukeMonty <31657111+LukeMonty@users.noreply.github.com>
2018-11-20 15:21:14 +0000

    Add files via upload

--- BlackJack.java
+++ BlackJack.java
@@ -2,30 +2,76 @@ package test;
 
 public class BlackJack {
 	
-	 
+	static int firstHand[] = new int[2];
+	static int secondHand[] = new int[2];
+	static int sumFirstHand = 0;
+	static int sumSecondHand = 0;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		blackJack();
+		
+		blackJackFirstHand();
+		blackJackSecondHand();
+		compareHands(sumFirstHand, sumSecondHand);
 	}
-	
-	public static void blackJack() {
-		int firstHand[] = new int[2];
-		int secondHand[];
-		int sum = 0;
+
+	public static int blackJackFirstHand() {
+
 		for (int i = 0; i < 2; i++) {
 			int range = (11 - 1) + 1;
-			
+
 			firstHand[i] = (int) (Math.random()*range) + 1;
-			System.out.println("Card " + (i + 1) + " is " +firstHand[i]);
-			
+			System.out.println("Hand 1: Card " + (i + 1) + " is " +firstHand[i]);
+
 		}
 		for (int i = 0; i < firstHand.length; i++) {
-			sum += firstHand[i];
+			sumFirstHand += firstHand[i];
+
+		}
+
+		System.out.println("First Hand total is " + sumFirstHand);
+
+
+		
+		return sumFirstHand;
+		
+	}
+	
+	public static int blackJackSecondHand() {
+		
+		for (int i = 0; i < 2; i++) {
+			int range = (11 - 1) + 1;
+
+			secondHand[i] = (int) (Math.random()*range) + 1;
+			System.out.println("Hand 2:Card " + (i + 1) + " is " +secondHand[i]);
+
+		}
+		for (int i = 0; i < secondHand.length; i++) {
+			sumSecondHand += secondHand[i];
+
+		}
+
+		System.out.println("Second Hand total is " + sumSecondHand);
+		
+		return sumSecondHand;
+	}
+	
+	
+	public static void compareHands(int sumFirstHand, int sumSecondHand) {
+		
+		if (sumFirstHand < sumSecondHand) {
+			
+			System.out.println("Second hand is bigger:" + sumSecondHand);
 			
 		}
-		
-		System.out.println("Hand total is " + sum);
+		if (sumFirstHand > sumSecondHand) {
+			
+			System.out.println("First hand is bigger:" + sumFirstHand);
+			
+		}
+		else if (sumFirstHand == sumSecondHand){
+			
+			System.out.println("Draw");
+		}
 	}
 
 }
